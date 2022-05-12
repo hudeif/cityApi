5342582144c8b6fcd63e3df922b9c8fd124fd1fe
hudeifa <hudeifagit@gmail.com>
2022-05-12 10:26:26 +0300

    first commit

---
+++ src/test/java/com/example/cityApi/CityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
