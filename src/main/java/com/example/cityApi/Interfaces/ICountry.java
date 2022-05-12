bf2681d687852981a0f08fce44f3eef31c464965
hudeifa <hudeifagit@gmail.com>
2022-05-12 11:13:19 +0300

    repositry

--- src/main/java/com/example/cityApi/Interfaces/ICountry.java
+++ src/main/java/com/example/cityApi/Interfaces/ICountry.java
@@ -1,4 +1,10 @@
 package com.example.cityApi.Interfaces;
 
-public interface ICountry {
+import com.example.cityApi.Models.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICountry extends JpaRepository<Country,Integer> {
+
 }
