38ddaff783ec571478555578eae0c7a0f8b9a6e9
Omar-nor <onor@uct.edu.so>
2022-05-12 11:15:30 +0300

    Addedd a Controller and Model Citry

---
+++ src/main/java/com/example/cityApi/Models/City.java
@@ -0,0 +1,20 @@
+package com.example.cityApi.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cityName;
+
+
+}
--- src/main/java/com/example/cityApi/Models/Country.java
+++ src/main/java/com/example/cityApi/Models/Country.java
@@ -1,4 +1,5 @@
 package com.example.cityApi.Models;
 
 public class Country {
+
 }
